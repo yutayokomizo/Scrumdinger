26bdd268dd2d8cc457da44dadffecee9c05bf05d
yutayokomizo <yutayoko.prime@gmail.com>
2022-08-25 21:26:42 +0900

    Initial Commit

---
+++ Scrumdinger/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Scrumdinger
+//
+//  Created by Yuta Yokomizo on 2022/08/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Yuta Yokomizo on 2022/08/25.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
